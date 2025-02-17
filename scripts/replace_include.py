761929f89175304bd9dbdcda80f3843a7592f31b
Shenghui Li <lishenghui.uu@gmail.com>
2025-02-17 10:52:59 +0100

    📝 Update

--- scripts/replace_include.py
+++ scripts/replace_include.py
@@ -5,7 +5,6 @@ import sys
 from pathlib import Path
 
 def replace_includes(file_path):
-    # 正则表达式匹配 .. include:: 指令
     include_pattern = re.compile(r'^\.\. include::\s*(.*)$')
 
     try:
@@ -16,7 +15,6 @@ def replace_includes(file_path):
         for line in lines:
             match = include_pattern.match(line.strip())
             if match:
-                # 获取包含的文件路径
                 include_file = match.group(1).strip()
                 include_path = (Path(file_path).parent / include_file).resolve()
 
@@ -34,7 +32,6 @@ def replace_includes(file_path):
         sys.exit(1)
 
 def main():
-    # 遍历 git 暂存区中的所有 .rst 文件
     readme_file = "../docs/README.rst"
 
     if readme_file.endswith('.rst'):
@@ -42,7 +39,6 @@ def main():
         with open("../README.rst", 'w') as file:
             file.writelines(updated_content)
 
-        # 重新加入 git 暂存区
         os.system(f'git add {readme_file}')
 
 if __name__ == "__main__":
